8b170878cb6742a4325e687a791526537be411b5
Ankit sharma <aksharma181007@gmail.com>
2025-06-22 22:49:08 +0530

    Update

--- CHAPTER2/absoluteValue.cpp
+++ CHAPTER2/absoluteValue.cpp
@@ -4,8 +4,11 @@ int main(){
     int n;
     cout<<"enter n :";
     cin>>n;
-    if(n>=0)
+    if(n>=0){
+    cout<<"The absolute value is: ";
     cout<<n;
+    }
+    
     else 
     cout<<-n;
 }
